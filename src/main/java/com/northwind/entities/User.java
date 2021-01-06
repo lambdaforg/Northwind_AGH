41096801f760636b32772ca3783df1d9a462f7e6
krwicher <kr.wicher@gmail.com>
2021-01-07 00:47:34 +0100

    dodanie klas mapujących dokumenty użytkowników i ról

---
+++ src/main/java/com/northwind/entities/User.java
@@ -0,0 +1,72 @@
+package com.northwind.entities;
+
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
+
+@Document(collection = "user")
+public class User {
+    @Id
+    @BsonProperty("id")
+    private int id;
+    @Indexed(unique = true)
+    private String email;
+    private String password;
+    private String fullName;
+    private boolean enabled;
+    @DBRef
+    private Set<Role> roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
